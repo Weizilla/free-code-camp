0549c49c7cd86264a3ece910cb01da6013626cd7
Wei Yang <wei@weizilla.com>
2016-05-26 22:18:44 -0500

    add main ui, button click animation and sounds

--- simon/simon.js
+++ simon/simon.js
@@ -1,7 +1,30 @@
 "use strict";
 
 $(document).ready(function () {
-   $("#svgTest").click(function() {
-      console.log("hello world"); 
-   })
+    var buttons = [
+        {id: "greenPad", down: "#00FF00", up: "#3CB371", sound: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3")},
+        {id: "redPad", down: "#FF0000", up: "#8B0000", sound: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3")},
+        {id: "yellowPad", down: "#FFFF00", up: "#DEB887", sound: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3")},
+        {id: "bluePad", down: "#1589FF", up: "#0000CD", sound: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3")},
+        {id: "startBtn", down: "#F9966B", up: "#C11B17"},
+        {id: "strictBtn", down: "#FFFFCC", up: "#FDD017"}
+    ];
+
+    buttons.forEach(function(button) {
+        $("#" + button.id).mousedown(function() {
+            $(this).attr("fill", button.down);
+            if (button.hasOwnProperty("sound")) {
+                button.sound.pause();
+                button.sound.play();
+            }
+            return false;
+        }).mouseup(function() {
+            $(this).attr("fill", button.up);
+            if (button.hasOwnProperty("sound")) {
+                button.sound.pause();
+            }
+            return false;
+        })
+    });
+
 });
