ba1a3e1662e2c5909a3e268a3b8e0ff47f1c575a
Wei Yang <wei@weizilla.com>
2016-06-10 12:23:58 -0500

    add handling of user input

--- simon/simon.js
+++ simon/simon.js
@@ -1,11 +1,14 @@
 "use strict";
 
 $(document).ready(function () {
-    var buttons = [
+    var padButtons = [
         {id: "#greenPad", down: "#00FF00", up: "#3CB371", index: 0, sound: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3")},
         {id: "#redPad", down: "#FF0000", up: "#8B0000", index: 1, sound: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3")},
         {id: "#yellowPad", down: "#FFFF00", up: "#DEB887", index: 2, sound: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3")},
-        {id: "#bluePad", down: "#1589FF", up: "#0000CD", index: 3, sound: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3")},
+        {id: "#bluePad", down: "#1589FF", up: "#0000CD", index: 3, sound: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3")}
+    ];
+
+    var otherButtons = [
         {id: "#startBtn", down: "#F9966B", up: "#C11B17"},
         {id: "#strictBtn", down: "#FFFFCC", up: "#FDD017"}
     ];
@@ -13,20 +16,29 @@ $(document).ready(function () {
     var sequence = [];
     var intervals = [];
     var running = false;
+    var userTimeoutInterval;
+    var userSequence = [];
+    var usersTurn = false;
 
-    buttons.forEach(function(button) {
+    padButtons.forEach(function(button) {
         $(button.id).mousedown(function() {
             $(this).attr("fill", button.down);
-            if (button.hasOwnProperty("sound")) {
-                button.sound.pause();
-                button.sound.play();
-            }
+            button.sound.pause();
+            button.sound.play();
             return false;
         }).mouseup(function() {
             $(this).attr("fill", button.up);
-            if (button.hasOwnProperty("sound")) {
-                button.sound.pause();
-            }
+            button.sound.pause();
+            return false;
+        })
+    });
+
+    otherButtons.forEach(function(button) {
+        $(button.id).mousedown(function() {
+            $(this).attr("fill", button.down);
+            return false;
+        }).mouseup(function() {
+            $(this).attr("fill", button.up);
             return false;
         })
     });
@@ -35,24 +47,53 @@ $(document).ready(function () {
         running = ! running;
         updateOnOffSwitch();
         if (! running) {
-            intervals.forEach(function (id) {
-                clearInterval(id);
-                //TODO reset all pad button states
-            });
+            reset();
         }
     });
-    
+
     //TODO what to do when start button is pressed and there's already a sequence
     $("#startBtn").click(function() {
         if (running) {
-            addSequence(); 
+            addSequence();
         }
     });
+
+    $(".padBtn").click(function() {
+        if (usersTurn) {
+            if (userTimeoutInterval) {
+                clearTimeout(userTimeoutInterval);
+            }
+            
+            var id = "#" + $(this).attr("id");
+            var buttonIdx = padButtons.find(function(button) {
+                return id === button.id;
+            }).index;
+            userSequence.push(buttonIdx);
+            checkValidUserSteps();
+        }
+    });
+
+    function checkValidUserSteps() {
+        console.log("checking user steps");
+        for (var i = 0; i < userSequence.length; i++) {
+            if (userSequence[i] !== sequence[i]) {
+                gameOver();
+                return;
+            }
+        }
+        console.log("all good");
+        if (userSequence.length === sequence.length) {
+            userSequence = [];
+            addSequence();
+        } else {
+            waitForUser();
+        }
+    }
 
     function playSequence(step) {
         console.log("playing step", step);
         var buttonIdx = sequence[step];
-        var button = buttons[buttonIdx];
+        var button = padButtons[buttonIdx];
         $(button.id).mousedown();
         intervals.push(setTimeout(function() {
             $(button.id).mouseup();
@@ -60,7 +101,7 @@ $(document).ready(function () {
                 playSequence(step + 1);
             } else {
                 console.log("done");
-                addSequence()
+                waitForUser();
             }
         }, 1000));
     }
@@ -73,11 +114,38 @@ $(document).ready(function () {
         }, 1000));
     }
 
+    function waitForUser() {
+        console.log("waiting for user");
+        if (userTimeoutInterval) {
+            clearTimeout(userTimeoutInterval);
+        }
+        var id = setTimeout(gameOver, 3000);
+        userTimeoutInterval = id;
+        intervals.push(id);
+        usersTurn = true;
+    }
+
+    function gameOver() {
+        console.log("game over!");
+        reset();
+    }
+
+    function reset() {
+        intervals.forEach(function(id) {
+            clearInterval(id);
+            //TODO reset all pad button states
+        });
+        usersTurn = false;
+        sequence = [];
+        userSequence = []; 
+        updateCount();
+    }
+
     function updateOnOffSwitch() {
         $("#onSwitch").attr("fill", running ? "green" : "black");
         $("#offSwitch").attr("fill", running ? "black" : "green");
     }
-    
+
     function updateCount() {
         $("#count").text(sequence.length);
     }
